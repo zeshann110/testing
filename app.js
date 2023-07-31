6b4504fa8b92594e1a95694c23dc4c22fe94600d
Zeshan Haider <76893463+zeshann110@users.noreply.github.com>
2023-07-31 11:19:58 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,34 +4,13 @@ const contractAddress = "0xb9B28b3B45Bfd1fFA2470a05caf83537C131f7d3";
 // Define the ABI (Application Binary Interface) of the contract
 const contractABI = [{"inputs":[{"internalType":"address","name":"_roflcoinTokenAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"AirdropClaimed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"airdropAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"airdropFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimAirdrop","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"hasReceivedAirdrop","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"roflcoinTokenAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newAmount","type":"uint256"}],"name":"updateAirdropAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newFee","type":"uint256"}],"name":"updateAirdropFee","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawBNB","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawROFL","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
+
 // Initialize web3
 let web3;
 let contract;
 
 async function initWeb3() {
-  // Modern dapp browsers
-  if (window.ethereum) {
-    web3 = new Web3(window.ethereum);
-    try {
-      // Request account access if needed
-      await window.ethereum.enable();
-    } catch (error) {
-      // User denied account access...
-      console.error("User denied account access");
-    }
-  }
-  // Legacy dapp browsers
-  else if (window.web3) {
-    web3 = new Web3(web3.currentProvider);
-  }
-  // Non-dapp browsers
-  else {
-    alert("You have to install the MetaMask extension or paste this URL https://roflcoin.xyz/airdrop into the TrustWallet browser.");
-    return;
-  }
-
-  // Load the contract
-  contract = new web3.eth.Contract(contractABI, contractAddress);
+  // ... (initWeb3 function remains unchanged)
 }
 
 // Function to connect the wallet
@@ -42,8 +21,106 @@ async function connectWallet() {
   }
 
   try {
+    // Show a popup to ask the user to choose a wallet
+    const selectedWallet = await askUserToChooseWallet();
+
+    if (!selectedWallet) {
+      alert("You have canceled the wallet connection.");
+      return;
+    }
+
+    if (selectedWallet === 'metamask') {
+      if (!window.ethereum) {
+        alert("MetaMask is required to connect the wallet.");
+        return;
+      }
+      web3 = new Web3(window.ethereum);
+    } else if (selectedWallet === 'trustwallet') {
+      if (!window.web3) {
+        alert("Trust Wallet is required to connect the wallet.");
+        return;
+      }
+      web3 = new Web3(web3.currentProvider);
+    }
+
     // Request account access if needed
     await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length > 0) {
+      alert("Wallet connected successfully!");
+    } else {
+      alert("No accounts found. Please check your wallet configuration.");
+    }
+  } catch (error) {
+    console.error("Failed to connect the wallet:", error);
+  }
+}
+
+// Function to show a popup and ask the user to choose a wallet
+async function askUserToChooseWallet() {
+  return new Promise((resolve) => {
+    const walletChoices = [
+      { name: 'MetaMask', value: 'metamask' },
+      { name: 'Trust Wallet', value: 'trustwallet' },
+    ];
+
+    const popupMessage = 'Please choose a wallet to connect:';
+    let popupContent = '<ul>';
+    walletChoices.forEach((wallet) => {
+      popupContent += `<li><button onclick="walletSelected('${wallet.value}')">${wallet.name}</button></li>`;
+    });
+    popupContent += '</ul>';
+
+    if (window.confirm(`${popupMessage}\n\n${popupContent}`)) {
+      resolve(selectedWallet);
+    } else {
+      resolve(null); // User canceled
+    }
+  });
+}
+
+// Function to handle the selected wallet from the popup
+function walletSelected(selectedWallet) {
+  if (selectedWallet === 'metamask') {
+    connectWithMetaMask();
+  } else if (selectedWallet === 'trustwallet') {
+    connectWithTrustWallet();
+  }
+}
+
+// Function to connect the wallet using MetaMask
+async function connectWithMetaMask() {
+  try {
+    if (!window.ethereum) {
+      alert("MetaMask is required to connect the wallet.");
+      return;
+    }
+    web3 = new Web3(window.ethereum);
+
+    // Request account access if needed
+    await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length > 0) {
+      alert("Wallet connected successfully!");
+    } else {
+      alert("No accounts found. Please check your wallet configuration.");
+    }
+  } catch (error) {
+    console.error("Failed to connect the wallet:", error);
+  }
+}
+
+// Function to connect the wallet using Trust Wallet
+async function connectWithTrustWallet() {
+  try {
+    if (!window.web3) {
+      alert("Trust Wallet is required to connect the wallet.");
+      return;
+    }
+    web3 = new Web3(web3.currentProvider);
+
+    // Request account access if needed
+    await window.web3.currentProvider.enable();
     const accounts = await web3.eth.getAccounts();
     if (accounts.length > 0) {
       alert("Wallet connected successfully!");
