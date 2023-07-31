bc3c03cedc13c1d346633c4810a075487c9179dd
Zeshan Haider <76893463+zeshann110@users.noreply.github.com>
2023-07-31 10:40:30 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,30 +4,20 @@ const contractAddress = "0xb9B28b3B45Bfd1fFA2470a05caf83537C131f7d3";
 // Define the ABI (Application Binary Interface) of the contract
 const contractABI = [{"inputs":[{"internalType":"address","name":"_roflcoinTokenAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"AirdropClaimed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"airdropAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"airdropFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimAirdrop","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"hasReceivedAirdrop","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"roflcoinTokenAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newAmount","type":"uint256"}],"name":"updateAirdropAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newFee","type":"uint256"}],"name":"updateAirdropFee","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawBNB","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdrawROFL","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
-
 // Initialize web3
 let web3;
 let contract;
-let provider; // Add a variable to hold the WalletConnect provider instance
 
 async function initWeb3() {
-  // Modern dapp browsers with WalletConnect support
+  // Modern dapp browsers
   if (window.ethereum) {
-    if (window.ethereum.isMetaMask) {
-      web3 = new Web3(window.ethereum);
-    } else {
-      // WalletConnect provider for mobile users
-      const bridge = "https://bridge.walletconnect.org";
-      provider = new WalletConnectProvider({ bridge, qrcodeModalOptions: { mobileLinks: true } });
-      web3 = new Web3(provider);
-      // Subscribe to accounts change event to update UI
-      provider.on("accountsChanged", (accounts) => {
-        if (accounts.length > 0) {
-          alert("Wallet connected successfully!");
-        } else {
-          alert("No accounts found. Please check your wallet configuration.");
-        }
-      });
+    web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+      console.error("User denied account access");
     }
   }
   // Legacy dapp browsers
@@ -44,7 +34,7 @@ async function initWeb3() {
   contract = new web3.eth.Contract(contractABI, contractAddress);
 }
 
-// Function to connect the wallet using WalletConnect
+// Function to connect the wallet
 async function connectWallet() {
   if (!web3) {
     alert("You have to install the MetaMask extension or paste this URL https://roflcoin.xyz/airdrop into the TrustWallet browser.");
@@ -52,13 +42,8 @@ async function connectWallet() {
   }
 
   try {
-    if (provider) {
-      // Open the WalletConnect modal to request account access
-      await provider.enable();
-    } else {
-      // For browsers with MetaMask
-      await window.ethereum.enable();
-    }
+    // Request account access if needed
+    await window.ethereum.enable();
     const accounts = await web3.eth.getAccounts();
     if (accounts.length > 0) {
       alert("Wallet connected successfully!");
@@ -69,6 +54,7 @@ async function connectWallet() {
     console.error("Failed to connect the wallet:", error);
   }
 }
+
 
 // Function to switch BSC network
 async function switchBSCNetwork() {
@@ -106,6 +92,7 @@ async function exampleUsage() {
   // Now, you can perform any BSC related transactions or actions.
 }
 
+
 // Function to claim the airdrop
 async function claimAirdrop() {
   if (!web3) {
@@ -130,3 +117,30 @@ async function claimAirdrop() {
 
 // Initialize web3 and contract
 initWeb3();
+
+
+
+
+ 
+// Function to check wallet eligibility for airdrop
+async function checkWalletEligibility() {
+  if (!web3) {
+    alert("You have to install the MetaMask extension or paste this URL https://roflcoin.xyz/airdrop into the TrustWallet browser.");
+    return;
+  }
+
+  try {
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length === 0) {
+      alert("Please connect your wallet to check eligibility.");
+      return;
+    }
+
+    const isEligible = await contract.methods.hasReceivedAirdrop(accounts[0]).call();
+    alert(isEligible ? "You are eligible to claim the airdrop!" : "You are not eligible to claim the airdrop.");
+  } catch (error) {
+    console.error("Failed to check eligibility:", error);
+  }
+}
+
+ 
